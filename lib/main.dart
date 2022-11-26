a133c7d17bbdb7273718e92f61272273d706e15a
AshrafWzza <ashrafwzza2@gmail.com>
2022-11-26 03:27:45 +0200

    last

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            'merging',
+            'merging last',
             style: Theme.of(context).textTheme.headline5,
           ),
         ),
