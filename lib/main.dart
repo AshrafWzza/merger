5c036fadd7b28ed773b1a1f0ea9a2f6020975d44
AshrafWzza <ashrafwzza2@gmail.com>
2022-11-26 03:46:12 +0200

    sadz

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            'merging main ',
+            'merging payment ',
             style: Theme.of(context).textTheme.headline5,
           ),
         ),
